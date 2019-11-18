0f52860e77cf4188e843b5f316627da67223b9db
102575814 <102575814@TC303-009>
2019-11-19 10:31:43 +1100

    added sample data

--- SVS_Database/SVS_Database/102575814Treatment.sql
+++ SVS_Database/SVS_Database/102575814Treatment.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[102575814Treatment]
 (
 	[Date] DATE NOT NULL, 
-    [Notes] NVARCHAR(50) NOT NULL, 
+    [Notes] NVARCHAR(100) NOT NULL, 
     [Price] INT NOT NULL, 
     [OwnerID] INT NOT NULL, 
     [PetName] NVARCHAR(50) NOT NULL, 
-    PRIMARY KEY ([Date], [OwnerID], [PetName]),
-	FOREIGN KEY ([PetName], [OwnerID]) references [dbo].[102575814Pet]
+	[ProcedureID] INT NOT NULL,
+    PRIMARY KEY ([Date], [OwnerID], [PetName], [ProcedureID]),
+	FOREIGN KEY ([PetName], [OwnerID]) references [dbo].[102575814Pet],
+	FOREIGN KEY ([ProcedureID]) references [dbo].[102575814Procedure]
 )
--- SVS_Database/SVS_Database/Script.PostDeployment1.sql
+++ SVS_Database/SVS_Database/Script.PostDeployment1.sql
@@ -10,10 +10,16 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
+DELETE FROM [102575814Treatment];
+DELETE FROM [102575814Pet];
+DELETE FROM [102575814Owner];
+DELETE FROM [102575814Procedure];
+
+
 Insert into [102575814Owner] ([OwnerID], [Surname], [Firstname], [Phone]) values 
 (1, 'Sinatra', 'Frank', 400111222),
 (2, 'Ellington', 'Duke', 400222333),
-(1, 'Fitzgerald', 'Ella', 400333444);
+(3, 'Fitzgerald', 'Ella', 400333444);
 
 Insert into [102575814Pet] ([PetName], [Type], [OwnerID]) values 
 ('Buster', 'Dog', 1),
@@ -21,3 +27,20 @@ Insert into [102575814Pet] ([PetName], [Type], [OwnerID]) values
 ('Stew', 'Rabbit', 2),
 ('Pooper', 'Dog', 3),
 ('Buster', 'Dog', 3);
+
+Insert into [102575814Procedure] ([ProcedureID], [Description], [Price]) values
+(1, 'Rabies Vaccination', 24),
+(5, 'Heart Worm Test', 25),
+(8, 'Tetnus Vaccination', 17),
+(10, 'Examine and Treat Wound', 30);
+
+Insert into [102575814Treatment] ([Date], [Notes], [Price], [OwnerID], [PetName], [ProcedureID]) values
+('2017-6-20', 'Routine Vaccination', 22, 1, 'Buster', 1),
+('2018-5-15', 'Booster Shot', 24, 1, 'Buster', 1),
+('2018-5-10', 'Wounds sustained in a apparent cat fight', 30, 1, 'Fluffy', 10),
+('2017-6-20', 'Routine Vaccination', 24, 1, 'Fluffy', 1),
+('2018-5-10', 'Wounds sustained during attempt to cook the stew', 30, 2, 'Stew', 10),
+('2018-5-15', 'Routine Vaccination', 24, 2, 'Stew', 1),
+('2018-5-18', 'Routine Test', 25, 3, 'Pooper', 5),
+('2017-6-20', 'Stepped on a Rusty Nail', 17, 3, 'Buster', 8),
+('2017-6-20', 'Routine Vaccination', 24, 3, 'Buster', 1);
